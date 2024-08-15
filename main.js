3843163221439c4e6ae59b3268f27152c6a71e78
Jaohe <107199273+Jaohe@users.noreply.github.com>
2024-08-15 09:17:11 -0300

    arquivo js criado

---
+++ main.js
@@ -0,0 +1,8 @@
+const numerosenha = document.querySelector(".parametro-senha__texto");
+let tamanhosenha = 12;
+numerosenha.texcontent = tamanhosenha;
+const letrasmaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letraminusculas = "abcdefghijklmnopqrstuvwxyz"
+const numeros = "0123456789"
+const simbulos = "!@%*/´"
+const botoes = document.querySelectorAll(".parametro")
